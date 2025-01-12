e50e92c530996a285f5598d5bfe31ce6f825411e
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-01-12 20:46:09 +0300

    Create elements.js

---
+++ scripts/elements.js
@@ -0,0 +1,12 @@
+// Footer
+footerContainer = document.querySelector("#footerContainer");
+const year = new Date().getFullYear();
+function footerContainerF(footerContainer) {
+  if (footerContainer != null) {
+    // Define footer content:
+    footerContainer.innerHTML += `<footer>Copyright © <a href="https://primesolar.github.io/web-developer/">Vladislav Kazantsev</a> ${year}</footer>`
+  }
+}
+footerContainerF(footerContainer);
+
+console.log("elements.js is completed");
